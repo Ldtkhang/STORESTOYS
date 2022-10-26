1bb2cf91844f0aa7d734f4728f1cf872b15e3bf1
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-10-27 02:39:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $order = "order.php";
 $logout = "logout.php";
 
 //////////////////////////////////////////////////// Connection ////////////////////////////////////////////////
-$host = "localhost";
-$username="root";
-$password="";
-$db= "myshop";
+$host = "ec2-52-23-131-232.compute-1.amazonaws.com";
+$username="qsvwvatlbtxgew";
+$password="11fd9a5621bb8b2411a2768e1bf6be30c145a863dc8eeb5517744c8988b16772";
+$db= "d1ah55t3276vdm";
 $conn = pg_connect('postgres://qsvwvatlbtxgew:11fd9a5621bb8b2411a2768e1bf6be30c145a863dc8eeb5517744c8988b16772@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d1ah55t3276vdm');
 include('theme.php');
