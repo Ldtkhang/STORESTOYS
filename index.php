939c7826c7e8d1d7a95ddf206d6180891a3bf14c
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-09 09:23:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 session_start();
 //$urladmin ="http://localhost:8080/STORE STOYS/admin/";
 //$urluser ="http://localhost:8080/STORE STOYS/";
-//$home = "home.php";
-//$register = "register.php";
-//$shoppingcart = "shoppingcart.php";
-//$order = "order.php";
-//$logout = "logout.php";
+$home = "home.php";
+$register = "register.php";
+$shoppingcart = "shoppingcart.php";
+$order = "order.php";
+$logout = "logout.php";
 
 //////////////////////////////////////////////////// Connection ////////////////////////////////////////////////
-//$host = "localhost";
-//$username="root";
-//$password="";
-//$db= "myshop";
+$host = "localhost";
+$username="root";
+$password="";
+$db= "myshop";
 $conn = pg_connect('postgres://qsvwvatlbtxgew:11fd9a5621bb8b2411a2768e1bf6be30c145a863dc8eeb5517744c8988b16772@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d1ah55t3276vdm');
 include('theme.php');
