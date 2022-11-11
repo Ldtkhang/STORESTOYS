099f6baf175a7833e838123867a6aefbba5eaa75
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-11 11:41:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,4 @@ session_start();
 //$password="";
 //$db= "myshop";
 $conn = pg_connect('postgres://qsvwvatlbtxgew:11fd9a5621bb8b2411a2768e1bf6be30c145a863dc8eeb5517744c8988b16772@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d1ah55t3276vdm');
-//include('theme.php');
-if (!$conn){
-    die("connection failed");
-}
-?>
+include('theme.php');
