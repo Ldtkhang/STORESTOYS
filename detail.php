fd35c97ad2d5cba62983354c7d885ff4b2959fa8
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-11 13:52:45 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -8,19 +8,6 @@ while ($row = pg_fetch_array($result)) {
     <div class="row main">
         <h2 class="product text-center">PRODUCT DETAILS</h2>
         <div class="image text-center">
-            <div class="image text-center">
-            <img class="card-img-top" src="admin/pimgs/01.jpg" alt="Card image cap" style="height:350px">
-
-            <h2 class="card-title">Police Patrol Ship</h2>
-            <p class="card-text">
-            </p><h3>Price: 48</h3>
-            <p></p>
-            <h3 class="description">Description:</h3>
-            <p class="description">
-            </p><h4>OMGGG</h4>
-            <p></p>
-            <a class="btn btn-primary mt-3" href="?page=cartadd.php&amp;id=1">Add to cart</a>
-        </div>
             <img class="card-img-top" src="<?php echo 'admin/pimgs/'
                                                 . $row['proimage']; ?>" alt="Card image cap" style="height:350px">
 
